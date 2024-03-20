4843ab85b6d2497016f68e6ee3cfd2cbefde0d8e
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-20 14:02:09 +0100

    ADD sort descending method

--- bubler_sort.rb
+++ bubler_sort.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 module ArrSort
-  def self.descending(arr) 
+  def self.descending(arr)
+    loop do
+      for i in 0..arr.length - 2 do
+        next unless arr[i] > arr[i + 1]
+
+        num = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = num
+        dishorder = true
+      end
+      break unless dishorder
+    end
+    arr
   end
 end
+
+print ArrSort.descending([2, 1, 20, 3, 4, 7, 3, 9, 0, -10])
