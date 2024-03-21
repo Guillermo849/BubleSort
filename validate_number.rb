6152ebef5b3fe409363fdec9fad9f46b71292eb5
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-21 15:25:15 +0100

    ADD Validate number method

--- validate_number.rb
+++ validate_number.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module ValidateNumber
+  IS_NOT_NUMERIC = Class.new(StandardError)
   def self.validate(num)
-    
+    return if num == '0'
+    raise IS_NOT_NUMERIC, 'The value given is not numeric' if num.to_i.zero?
   end
 end
