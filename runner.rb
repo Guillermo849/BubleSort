6f2d159c518e3e2da80d723d994cb338afd924e4
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-25 10:01:32 +0100

    MOD run method loop and use of Module Validatenumber

--- runner.rb
+++ runner.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
+require './validate_number'
+
 class Runner
-    def self.run
+  include ValidateNumber
+  def run
+    arr = []
+    answer = 'Y'
+    while answer == 'Y'
+      begin
+        puts 'Insert a number'
+        num = gets.chomp
+        validate(num)
+        arr.push(num.to_i)
+        puts 'Would you like to add another number?(Y/N)'
+        answer = gets.chomp.upcase
+      rescue ValidateNumber::IS_NOT_NUMERIC => e
+        puts e.message
+      end
     end
+  end
 end
 
+Runner.new.run
