81a425f6fcd426a9172f0c20bf1b5eea328504c1
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-25 09:58:53 +0100

    MOD validate class method to method

--- validate_number.rb
+++ validate_number.rb
@@ -2,7 +2,7 @@
 
 module ValidateNumber
   IS_NOT_NUMERIC = Class.new(StandardError)
-  def self.validate(num)
+  def validate(num)
     return if num == '0'
     raise IS_NOT_NUMERIC, 'The value given is not numeric' if num.to_i.zero?
   end
