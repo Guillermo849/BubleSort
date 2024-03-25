1036549a974bb91f74b2424a13dfc4bbbb7886ca
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-25 12:54:30 +0100

    MOD Sorting method loop logic

--- service/bubble_sort.rb
+++ service/bubble_sort.rb
@@ -2,20 +2,15 @@
 
 module BubbleSort
   def self.sort(arr)
-    until check_sorted(arr)
-      arr.each_with_index do |_number, index|
-        next if arr[index + 1].nil?
-        next unless arr[index] > arr[index + 1]
+    (0...arr.size).each do |num1|
+      (0..arr.size - 1).each do |num2|
+        next unless arr[num2] > arr[num1]
 
-        num = arr[index]
-        arr[index] = arr[index + 1]
-        arr[index + 1] = num
+        num = arr[num1]
+        arr[num1] = arr[num2]
+        arr[num2] = num
       end
     end
     arr
-  end
-
-  def self.check_sorted(arr)
-    arr.each_cons(2).all? { |a| (a[0] <=> a[1]) <= 0 }
   end
 end
