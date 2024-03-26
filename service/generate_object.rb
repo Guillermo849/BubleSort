3c9ed5f8f0c1231c1010cc7951f969a808e4bb02
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-26 14:44:23 +0100

    ADD generate array method

--- service/generate_object.rb
+++ service/generate_object.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module GenerateObject
-    def self.GenerateArray(size: , small_number:, big_number:)
-    end
+  def self.generate_array(size:, small_number:, big_number:)
+    arr = []
+    (0...size).each { |i| arr[i] = rand(small_number..big_number) }
+
+    arr
+  end
 end
