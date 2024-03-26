177ac6173d51760e52ef013ddbc024afeae18c04
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-26 09:39:41 +0100

    MOD move Error class to Runner class AND change method name and logic

--- service/validation.rb
+++ service/validation.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
 module Validation
-  NotIntegerError = Class.new(StandardError)
-
   private
 
-  def validate_integer(num)
-    return true if num == '0'
-    return true unless num.to_i.zero?
-
-    false
+  def validate_input_char?(num)
+    true unless num.to_i.zero? && num != '0'
   end
 end
