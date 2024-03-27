14bd9207a4a909a77b5049f8b1f5f2a62a6cf81e
Guillermo <orivealonsoguillermo@gmail.com>
2024-03-27 11:57:09 +0100

    MOD Refactor run logic into methods and array generation logic

--- service/runner.rb
+++ service/runner.rb
@@ -10,45 +10,66 @@ class Runner
   BadRangeError = Class.new(StandardError)
 
   def run
-    arr = []
-    puts 'Generate Array automated or manually?(1/2)'
-    if gets.chomp == '1'
-      size = ask_input(question: 'Input the size of the Array')
-      start_of_range = ask_input(question: 'Input the first number for the range of numbers')
-      end_of_range = ask_input(question: 'Input the last number for the range of numbers')
-
-      if start_of_range > end_of_range
-        raise BadRangeError,
-              'Bad range, start of range is bigger than the end of range'
-      end
-
-      arr = generate_array(size: size, start_num_of_range: start_of_range, end_num_of_range: end_of_range)
-    else
-      answer = 'Y'
-      while answer == 'Y'
-        begin
-          puts 'Insert a number'
-          num = gets.chomp
-          raise NotIntegerError, 'The value is not an Integer' unless validate_input_char?(num)
-
-          arr.push(num.to_i)
-          puts 'Would you like to add another number?(Y/N)'
-          answer = gets.chomp.upcase
-        rescue NotIntegerError => e
-          puts e.message
-        end
+    answer = 'Y'
+    while answer == 'Y'
+      begin
+        print SortAlgorithm.bundle_sort(generate_array)
+        puts 'Would you like '
+        answer = gets.chomp.upcase
+        @array_generation_option = nil
+      rescue BadRangeError, NotIntegerError => e
+        puts e.message
       end
     end
-    print SortAlgorithm.bundle_sort(arr)
   end
 
   private
 
-  def ask_input(question:)
+  def generate_array
+    return automated_array if @array_generation_option
+    return manually_array if @array_generation_option == false
+
+    @array_generation_option = true
+    puts 'Type A for automated array generation or any key for manually?'
+    return automated_array if gets.chomp == 'A'
+
+    @array_generation_option = false
+    manually_array
+  end
+
+  def manually_array
+    arr = []
+    answer = 'Y'
+    while answer == 'Y'
+      puts 'Insert a number'
+      num = gets.chomp
+      raise NotIntegerError, 'The value is not an Integer' unless validate_input_char?(num)
+
+      arr.push(num.to_i)
+      puts 'Would you like to add another number?(Y/N)'
+      answer = gets.chomp.upcase
+    end
+    arr
+  end
+
+  def automated_array
+    size = ask_input_integer_number(text: 'Input the size of the Array')
+    start_of_range = ask_input_integer_number(text: 'Input the first number for the range of numbers')
+    end_of_range = ask_input_integer_number(text: 'Input the last number for the range of numbers')
+
+    if start_of_range > end_of_range
+      raise BadRangeError,
+            'Bad range, start of range is bigger than the end of range'
+    end
+
+    Array.new(size) { rand(start_of_range..end_of_range) }
+  end
+
+  def ask_input_integer_number(text:)
     values_correct = false
     until values_correct
       begin
-        puts question
+        puts text
         num = gets.chomp
         raise NotIntegerError, 'The value is not an Integer' unless validate_input_char?(num)
 
@@ -58,9 +79,5 @@ class Runner
       end
     end
     num.to_i
-  end
-
-  def generate_array(size:, start_num_of_range:, end_num_of_range:)
-    Array.new(size) { rand(start_num_of_range..end_num_of_range) }
   end
 end
